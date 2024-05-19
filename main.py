6cdd7af1f297b3c092a70528160b0c206284416b
trisharoy37 <25trisharoy@gmail.com>
2024-05-19 22:13:01 +0530

    'New commit'

--- main.py
+++ main.py
@@ -136,5 +136,5 @@ def announcements():
     return jsonify(list1), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
     
